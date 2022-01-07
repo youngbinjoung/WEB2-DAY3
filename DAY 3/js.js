29a1a1a6809a767265c2f158b736305dc50b1342
Youngbin Jung <71386343+youngbinjoung@users.noreply.github.com>
2022-01-07 18:04:03 +0900

    Add files via upload

---
+++ DAY 3/js.js
@@ -0,0 +1,26 @@
+// 주어.동사(목적어);
+// 콘솔화면을 깨끗이 지운다.
+console.clear();
+
+console.log(15);
+// console.log(t);
+// 콘솔에 값을 출력한다.
+
+// var 키워드는 변수를 만들때 한!번!만! 사용한다.
+
+var t = "사과"; 
+
+console.log(t);
+
+// 변수의 값은 바뀔 수 있다.
+// x = 3
+
+t = 15;
+
+console.log(t);
+
+var x = 2;
+var y = 5;
+
+console.log(x - y);
+console.log("안녕");
---
+++ DAY 3/js_test.js
@@ -0,0 +1,14 @@
+
+var dan=2;
+
+console.log(dan+" X "+1+" = "+dan*1);
+console.log(dan+" X "+2+" = "+dan*2);
+console.log(dan+" X "+3+" = "+dan*3);
+console.log(dan+" X "+4+" = "+dan*4);
+console.log(dan+" X "+5+" = "+dan*5);
+console.log(dan+" X "+6+" = "+dan*6);
+console.log(dan+" X "+7+" = "+dan*7);
+console.log(dan+" X "+8+" = "+dan*8);
+console.log(dan+" X "+9+" = "+dan*9);
+
+document.write("안녕하세요");
